a29aec60524fcc82708a3007d25d84ea5a4aaece
Nathen Harvey <nharvey@chef.io>
2014-12-04 21:05:12 +0000

    update the default apache recipe

--- chef-repo/cookbooks/apache/recipes/default.rb
+++ chef-repo/cookbooks/apache/recipes/default.rb
@@ -7,3 +7,14 @@
 package "httpd" do
   action :install
 end
+
+service "httpd" do
+  action [:start, :enable]
+end
+
+template "/var/www/html/index.html" do
+  source "index.html.erb"
+  mode   "0644"
+  owner  "root"
+  group  "root"
+end
