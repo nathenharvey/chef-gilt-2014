60dc2c35adf4e58ed3944d35c675845f60d0a660
Nathen Harvey <nharvey@chef.io>
2014-12-04 17:25:22 +0000

    initial motd cookbook

---
+++ chef-repo/cookbooks/motd/metadata.rb
@@ -0,0 +1,8 @@
+name             'motd'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures motd'
+long_description 'Installs/Configures motd'
+version          '0.1.0'
+
