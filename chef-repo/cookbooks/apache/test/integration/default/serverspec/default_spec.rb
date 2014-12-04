80d1b972bf1cf0e4f3166eccee3189a31b6b4273
Nathen Harvey <nharvey@chef.io>
2014-12-04 20:32:21 +0000

    Add some integration tests

--- chef-repo/cookbooks/apache/test/integration/default/serverspec/default_spec.rb
+++ chef-repo/cookbooks/apache/test/integration/default/serverspec/default_spec.rb
@@ -5,4 +5,21 @@ describe 'apache' do
   it "is awesome" do
     expect(true).to eq true
   end
+  
+  it "is installed" do
+    expect(package "httpd").to be_installed
+  end
+
+  it "is running"
+
+  it "is responding to http requests"
+
+  it "is displaying the proper home page"
+
+  it "is running on the default port"
 end
+
+
+
+
+
