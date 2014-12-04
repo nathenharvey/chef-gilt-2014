98c9ccf5b42a9f169a837c597350c497be13d4c6
Nathen Harvey <nharvey@chef.io>
2014-12-04 18:39:45 +0000

    add initial ntp cookbook

---
+++ chef-repo/cookbooks/ntp/README.md
@@ -0,0 +1,4 @@
+# ntp
+
+TODO: Enter the cookbook description here.
+
---
+++ chef-repo/cookbooks/ntp/metadata.rb
@@ -0,0 +1,8 @@
+name             'ntp'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures ntp'
+long_description 'Installs/Configures ntp'
+version          '0.1.0'
+
