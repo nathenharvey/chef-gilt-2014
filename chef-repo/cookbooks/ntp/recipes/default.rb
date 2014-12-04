6a364ac2470ee8dfbfe828c739ab9b813a3c2fcf
Nathen Harvey <nharvey@chef.io>
2014-12-04 19:19:43 +0000

    some more ntp stuff

--- chef-repo/cookbooks/ntp/recipes/default.rb
+++ chef-repo/cookbooks/ntp/recipes/default.rb
@@ -3,3 +3,38 @@
 # Recipe:: default
 #
 # Copyright (c) 2014 The Authors, All Rights Reserved.
+
+service "ntpd" do
+  action [:start, :enable]
+end
+
+["ntp", "ntpdate"].each do |package_name|
+  package package_name
+end
+
+template "/etc/ntp.conf" do
+  notifies :restart, "service[ntpd]"
+  source "ntp.conf.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  action :create
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+# package "ntp" 
+# 
+# package "ntpdate"
+#
+
